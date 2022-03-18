5ab7dfb617c2e99c9d9dbeefce2956bc165cac1e
adamromero <adamromero89@gmail.com>
2022-03-18 16:05:14 -0700

    Adjust filepath to dist

--- server/server.js
+++ server/server.js
@@ -18,10 +18,12 @@ app.use("/api/login", require("./routes/userAdmin"));
 app.use("/api/aws-upload", require("./routes/aws"));
 
 if (process.env.NODE_ENV === "production") {
-   app.use(express.static(path.join(__dirname, "/client/dist")));
+   app.use(express.static(path.join(__dirname, "../client/dist")));
 
    app.get("*", (req, res) =>
-      res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+      res.sendFile(
+         path.resolve(__dirname, "../", "client", "dist", "index.html")
+      )
    );
 }
 
